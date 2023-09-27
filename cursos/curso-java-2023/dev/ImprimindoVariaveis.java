006c260b7d2d75b3e199dbb4e0b5bb653371afda
alanjoffre <ajma.keisaa@gmail.com>
2023-09-27 20:33:23 -0300

    update tipos-primitivos commit

--- cursos/curso-java-2023/dev/ImprimindoVariaveis.java
+++ cursos/curso-java-2023/dev/ImprimindoVariaveis.java
@@ -3,9 +3,19 @@ public class ImprimindoVariaveis {
     public static void main(String[] args){
         int idade = 10; // Variável sempre começa com letra minuscula
         int idadeDosPaisPAraCadastro = 40; // primeira letra minuscula e o restante maiuscula
+        double salarioDoube = 3000;
+        float salarioFloat = 3000;
+        byte idadeByte = 12;
+        short idadeShort = 32767;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        long numeroGrande = 1000L;
+        char caractere = 'A';
+        String nome = "William";
         System.out.println(idade);
         System.out.println(idadeDosPaisPAraCadastro);
         System.out.println("A idade é: "+idadeDosPaisPAraCadastro);
+        System.out.println(nome+" de idade "+idade);
     }
     
 }
